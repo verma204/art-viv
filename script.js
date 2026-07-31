ff13ac7a51135cf780f038906bcb243a5500c3cf
verma204 <verma204@purdue.edu>
2026-07-30 22:08:03 -0400

    Added arrows to background

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ window.addEventListener('mousemove', (e) => {
     mouse.y = e.clientY;
 });
 
+/*
 function animate() {
     //Clear screen
     ctx.clearRect(0, 0, width, height);
@@ -27,7 +28,9 @@ function animate() {
     //Calculate distance from center point and angle the arrow should be pointed
     const dx = mouse.x - centerX;
     const dy = mouse.y - centerY;
-    const angle = Math.atan2(dy, dx);
+    const angle = Math.atan2(dy, dx) + (Math.PI);
+
+
 
     //save state before rotation
     ctx.save();
@@ -56,6 +59,64 @@ function animate() {
     ctx.restore();
 
     requestAnimationFrame(animate);
+}
+*/
+
+function animate() {
+    ctx.clearRect(0, 0, width, height);
+
+    //Calculate center point
+
+    /*
+    const centerX = width / 2;
+    const centerY = height / 2;
+    drawArrow(centerX, centerY);
+
+    const x2 = 600;
+    const y2 = 600;
+    drawArrow(x2, y2);
+    */
+    populateArrows();
+    
+    requestAnimationFrame(animate);
+     
+}
+
+function populateArrows(){
+    for (let i = 0; i <= 20; i++) {
+        for (let j = 0; j <= 10; j++) {
+            drawArrow(100 * i, 100 * j);
+        }
+    }
+}
+
+function drawArrow(x, y) {
+    const dx = mouse.x - x;
+    const dy = mouse.y - y;
+    const angle = Math.atan2(dy,dx)
+    
+    ctx.save();
+
+    ctx.translate(x, y)
+    ctx.rotate(angle);
+    
+    const arrowLength = 50;
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(arrowLength, 0);
+
+    ctx.lineTo(arrowLength - 15, -8);
+    ctx.moveTo(arrowLength, 0);
+    ctx.lineTo(arrowLength - 15, 8);
+
+    //Fill in arrow
+    ctx.strokeStyle = 'rgba(0, 220, 255, 0.8)';
+    ctx.lineWidth = 3;
+    ctx.stroke();
+
+    ctx.restore();
+
 }
 
 animate();
